bb6a422e61adb9cd935450c025bf6e658c572a47
Florian meyer <florian.meyer@stinger-soft.net>
2015-06-05 19:59:03 +0200

    Checkin test

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Stinger Media Parser package.
+ *
+ * (c) Oliver Kotte <oliver.kotte@stinger-soft.net>
+ * (c) Florian Meyer <florian.meyer@stinger-soft.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace StingerSoft\MediaParserBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration  implements ConfigurationInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getConfigTreeBuilder()
+	{
+		$treeBuilder = new TreeBuilder();
+		$rootNode = $treeBuilder->root('stinger_soft_audio_parser');
+
+
+		return $treeBuilder;
+	}
+}
